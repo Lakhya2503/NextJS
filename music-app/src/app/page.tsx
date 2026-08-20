986dafeb6d3d8b904bff86104ffd0439d45ee6ad
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-08-20 11:19:25 +0530

    feat: implement HeroSection component and enhance Home page layout

--- music-app/src/app/page.tsx
+++ music-app/src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-        <h2 className="text-center " >CODE WITH CAFFINE</h2>
+        <h2 className="text-center text-2xl font-bold" >CODE WITH CAFFINE</h2>
+        <HeroSection/>
       </main>
   );
 }
--- music-app/src/components/HeroSection.tsx
+++ music-app/src/components/HeroSection.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className="h-auto md:h-[40rem] rounded-md flex flex-col md:flex-row justify-center items-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <div className="p-4 relative z-10 w-full text-center">
+            <h1>MASTER THE ART OF MUSIC</h1>
+            <p>Dive</p>
+        </div>
+    </div>
   )
-}
+}   
 
 export default HeroSection
